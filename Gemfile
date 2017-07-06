9b6a309dd4235ae7e4c6942dc6f984035d13c4ff
zero0n3 <ilsimonone@gmail.com>
2017-07-06 14:22:29 +0200

    modifiche varie visive css

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+#paperclip
+gem "paperclip", "~> 5.0.0"
 #devise
 gem 'devise'
 #jquery-rails
--- app/models/content.rb
+++ app/models/content.rb
@@ -1,3 +1,12 @@
 class Content < ApplicationRecord
   belongs_to :user
+
+  has_attached_file :cover, styles: { medium: "300x>", thumb: "100x>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :cover, content_type: /\Aimage\/.*\z/,
+  message: "solo immagini sono supportate"
+
+  has_attached_file :allegato
+  validates_attachment_content_type :allegato, :content_type => ['application/pdf', 'audio/mp3'],
+  message: "Formato non supported"
+
 end
